f1140ad4c7117458ba943451ddd315a46b17a33d
Philipp Wagner <wagner.arnstadt@googlemail.com>
2018-02-17 20:34:17 +0100

    using factory function to get custom element constructor

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 console.log('get my-element', customElements.get('my-element'));
 console.log('get my-second-element', customElements.get('my-second-element'));
 
-function MyElement() {
-  return HTMLElement.call(this)
+// Using factory function to get Constructor for custom elements
+function getConstructor() {
+  var MyElement = function () {
+    return HTMLElement.call(this)
+  }
+  Object.setPrototypeOf(MyElement.prototype, HTMLElement.prototype);
+  Object.setPrototypeOf(MyElement, HTMLElement);
+
+  MyElement.prototype.attributeChangedCallback = function (name, old, value) {
+      this.value = value;
+      console.log('Attribute changed', name, old, value);
+      // alert(value);
+  };
+  MyElement.observedAttributes = ['value'];
+
+  return MyElement
 }
 
-Object.setPrototypeOf(MyElement.prototype, HTMLElement.prototype);
-Object.setPrototypeOf(MyElement, HTMLElement);
-
-MyElement.prototype.attributeChangedCallback = function (name, old, value) {
-    this.value = value;
-    console.log('Attribute changed', name, old, value);
-    // alert(value);
-};
-MyElement.observedAttributes = ['value'];
-
-
-// document.registerElement('my-element', ElementClass);
-customElements.define('my-element', MyElement);
+customElements.define('my-element', getConstructor());
 console.log('registered my-element');
 console.log('get my-element', customElements.get('my-element'));
 console.log('get my-second-element', customElements.get('my-second-element'));
-customElements.define('my-second-element', MyElement);
+customElements.define('my-second-element', getConstructor());
 console.log('registered my-second-element');
 console.log('get my-element', customElements.get('my-element'));
 console.log('get my-second-element', customElements.get('my-second-element'));
